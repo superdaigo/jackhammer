2f12857d311f4c701688f7aad1e96fd7949db19f
Daigo Sakamoto <daigo@treasure-data.com>
2017-05-19 18:38:00 +0900

    Clean up docker-compose files

--- docker-build.sh
+++ docker-build.sh
@@ -1,7 +1,5 @@
-docker-compose build
-sleep 30
-docker-compose run web rake db:create
-docker-compose run web rake db:migrate
-docker-compose run  -e APPLICATION_MODE=$1 web rake db:seed
-docker-compose up -d
-docker-compose run web figlet -k -w 500 "JACKHAMMER IS UP" 
+docker-compose create --build
+docker-compose start
+docker-compose exec web rake db:schema:load
+docker-compose run --rm -e APPLICATION_MODE=$1 web rake db:seed
+docker-compose exec web figlet -k -w 500 "JACKHAMMER IS UP"
