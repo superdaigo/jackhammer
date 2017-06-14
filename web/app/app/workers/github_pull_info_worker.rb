5a410695905ef3d32be97ffc9e75b20e2a0a67df
Daigo Sakamoto <daigo@treasure-data.com>
2017-06-14 20:50:31 +0900

    Fix: fetch github repositories

--- web/app/app/workers/github_pull_info_worker.rb
+++ web/app/app/workers/github_pull_info_worker.rb
@@ -4,6 +4,7 @@ class GithubPullInfoWorker
 	sidekiq_options unique: :while_executing
 	sidekiq_options({ :queue => :gitpull,:retry => 2 })
 	def perform
+		Octokit.auto_paginate = true
 		Dir.mkdir(Rails.root + "log/scans") unless File.exists?(Rails.root + "log/scans")
 		@logfile = File.open(Rails.root.join("log/scans/github_pull.log"), 'a')
 		raise StandardError,"Github details are not configured" unless Setting['github']
@@ -19,38 +20,41 @@ class GithubPullInfoWorker
 				team = Team.find_or_create_by(name: git_team[:name])
 				@logfile.puts "pulling teams info of #{git_team[:name]}..and team id#{git_team[:id]}"
 				team_repos = client.team_repositories(git_team[:id])
-				team_repos.each do |project| 
+				team_repos.each do |project|
 					repo = Repo.where(name: project[:name]).first_or_initialize.tap do |repo|
-						repo.ssh_repo_url = project[:clone_url]
-						repo.team = team
-						repo.git_type = "github"
-						repo.repo_type = "Static"
-						repo.save
-					end
-					branches = client.branches(project[:owner][:id])
-					repo_branches = repo.branches.map(&:name)
-					branches.each do |branch|
-						if repo_branches.include?(branch.name)
-							puts "#{branch.name} is existing..."
-						elsif branch[:name]!= 'gh-pages' && if branch[:name]!= 'v128'
-							branch = Branch.create!(name: branch.name,repo: repo) 
-							puts "branch name....#{branch.name}"
-						else
-
+						begin
+							repo.ssh_repo_url = project[:clone_url]
+							repo.team = team
+							repo.git_type = "github"
+							repo.repo_type = "Static"
+							repo.save
+						end
+						begin
+							branches = client.branches(project[:full_name])
+							repo_branches = repo.branches.map(&:name)
+							branches.each do |branch|
+								if repo_branches.include?(branch.name)
+									puts "#{branch.name} is existing..."
+								elsif branch[:name]!= 'gh-pages' && branch[:name]!= 'v128'
+									branch = Branch.create!(name: branch.name,repo: repo)
+									puts "branch name....#{branch.name}"
+								end
+							end
+						rescue Octokit::NotFound => e
+							@logfile.puts "no branch: team:#{git_team[:name]} repo:#{project[:name]} #{e.inspect}"
 						end
 					end
 				end
+			rescue Exception=>e
+				@logfile.puts "Some exception is occured...#{e.inspect}"
 			end
-		rescue Exception=>e
-			@logfile.puts "Some exception is occured...#{e.inspect}"
-		end
-		AlertNotification.create(user_id: Setting['gitlab']['user_id'].to_i,identifier: 'task',alert_type: 'success',message: 'Pulling git info done!',task_name: "Git pull")
+			AlertNotification.create(user_id: Setting['gitlab']['user_id'].to_i,identifier: 'task',alert_type: 'success',message: 'Pulling git info done!',task_name: "Git pull")
 	  end
 		@logfile.close
 	rescue StandardError => e
-	@logfile.puts e.message
-	AlertNotification.create(user_id: Setting['github']['user_id'].to_i,identifier: 'task',alert_type: 'danger',message: e.message,task_name: "Git pull")
-	@logfile.close
-	raise e
-    end
+		@logfile.puts e.message
+		AlertNotification.create(user_id: Setting['github']['user_id'].to_i,identifier: 'task',alert_type: 'danger',message: e.message,task_name: "Git pull")
+		@logfile.close
+		raise e
+	end
 end
